1219ab0270ca8e0247d33ff832a142d772c465f9
Nicolás Salazar <ngongora@smarthop.co>
2021-03-14 14:39:40 -0500

    Base project config

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+console.log(chalk.white.bold('Started index.js 🥸'));
+
+// My code goes here:
+// ~~~~~~~~~~~~~~~~~~
+
+console.log(chalk.white.bold('Finished index.js 🧐'));
